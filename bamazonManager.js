6fc97909ef47cf2feca8e1ebd07dbe31024d3567
Simmie <skajogbola@users.noreply.github.com>
2019-04-30 00:06:25 -0400

    commit

--- bamazonManager.js
+++ bamazonManager.js
@@ -40,17 +40,17 @@ function start() {
                     lowInventory();
                     break;
 
-                // case "Add to Inventory":
-                //     addInventory();
-                //     break;
+                case "Add to Inventory":
+                    addInventory();
+                    break;
 
-                // case "Add New Product":
-                //     addProduct();
-                //     break;
+                case "Add New Product":
+                    addProduct();
+                    break;
 
-                // case "exit":
-                //     connection.end();
-                //     break;
+                case "exit":
+                    connection.end();
+                    break;
             }
         });
 }
@@ -66,13 +66,81 @@ function productForSale() {
 }
 
 function lowInventory() {
-    connection.query("SELECT * FROM products WHERE stock_quantity < 190", 
-    function (err, res) {
-        if (err) throw err;
-        for (var i = 0; i < res.length; i++) {
-            console.log("Low Inventory!!!");
-            console.log("ids:" + res[i].item_id + "   ||   " + "Name:" + res[i].product_name);
-        }
-        start();
-    });
+    connection.query("SELECT * FROM products WHERE stock_quantity <= 5",
+        function (err, res) {
+            if (err) throw err;
+            for (var i = 0; i < res.length; i++) {
+                console.log("============================================= Low Bamazon Inventory (5 or Less in Stock) ===============================================");
+                console.log("ids:" + res[i].item_id + "   ||   " + "Name:" + res[i].product_name);
+            }
+            start();
+        });
+}
+
+function addInventory() {
+    inquirer
+        .prompt([
+            {
+                name: "productID",
+                type: "input",
+                message: "Please enter the ID number you would like to add to inventory?"
+            },
+            {
+                name: "productNumber",
+                type: "input",
+                message: "How many units of this item would you like to add?"
+            },
+        ])
+        .then(function (answer) {
+            connection.query(
+                "UPDATE products SET stock_quantity=? WHERE item_id =?",
+                [answer.productNumber, answer.productID],
+                function (err, res) {
+                    if (err) throw err;
+                    console.log("======Item has been sucessfully updated to your inventory=======");
+                    start();
+                });
+        })
+}
+
+function addProduct() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Please enter the item name of the new product."
+            },
+            {
+                type: "input",
+                name: "department",
+                message: "Please enter which department name of which the new product belongs.",
+            },
+            {
+                type: "input",
+                name: "price",
+                message: "Please enter the price of the new product (0.00).",
+            },
+            {
+                type: "input",
+                name: "quantity",
+                message: "Please enter the stock quantity of the new product.",
+            }
+        ])
+        .then(function (answer) {
+            connection.query(
+                "INSERT INTO products SET ?",
+                {
+                    product_name: answer.name,
+                    department_name: answer.department,
+                    price: answer.price,
+                    stock_quantity: answer.quantity
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.log("Your item: " + answer.name +" has been added sucessfully!!");
+                    start();
+                }
+            );
+        });
 }
