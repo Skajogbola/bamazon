6145191938789cf395478603d70a2cd7def5a533
Simmie <skajogbola@users.noreply.github.com>
2019-04-30 21:43:23 -0400

    commit

--- bamazonManager.js
+++ bamazonManager.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Folaranmi1@5252",
+    password: "root",
     database: "bamazon"
 });
 
